8ef2cd82b1373767984a327189336f5e52938f8a
Fentend <fentendmarkettech@gmail.com>
2023-12-19 12:20:03 +0100

    link added to button

--- src/Login.jsx
+++ src/Login.jsx
@@ -29,7 +29,7 @@ function Login () {
                         <input type="email" placeholder="Username" name="user_name" required className="md:w-auto w-auto md:mx-3 mx-1 rounded-sm border border-t-2 border-l-2 border-[#8D8C8F] placeholder-[#8D8C8F] placeholder:font-semibold pl-1" />
                         <input type="password" placeholder="Password" name="user_password" required className="md:w-auto w-auto md:mx-3 mx-1 md:my-2 my-1 rounded-sm border border-t-2 border-l-2 border-[#8D8C8F] placeholder-[#8D8C8F] placeholder:font-semibold pl-1"/>
                         <button type="submit" className="bg-[#5F3C7F] text-white text-center md:text-sm text-xs md:font-semibold md:rounded-3xl rounded-xl py-1 md:px-3 px-1 md:mx-36 mx-16 md:mb-5 mb-2">Log in</button>
-                        <h6 className="text-xs text-[#B7B7B7] text-left md:mx-5 mx-1 font-sans">Need help? Call Socket Technical Support at 1-800-762-5383. We'll be happy to walk you through the new site.</h6>
+                        <h6 className="text-[10px] text-[#B7B7B7] text-left md:mx-5 mx-1 font-sans">Need help? Call Socket Technical Support at 1-800-762-5383. We'll be happy to walk you through the new site.</h6>
                     </form>
                     <p className="text-center text-xs text-[#4F2471] underline mt-6 cursor-pointer">View Mobile Version</p>
                 </div>
